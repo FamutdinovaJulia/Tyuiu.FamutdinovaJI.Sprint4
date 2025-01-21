85988bd2a81b2e1572d70871de78260304fa4033
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-22 00:51:36 +0500

    Add project files.

---
+++ Tyuiu.FamutdinovaJI.Sprint4.Task0.V22.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.FamutdinovaJI.Sprint4.Task0.V22.Lib
+{
+    public class DataService : ISprint4Task0V22
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            { 
+                if (array[i] % 2 != 0)
+                {
+                    sum += array[i];
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.FamutdinovaJI.Sprint4.Task0.V22/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint4.Task0.V22.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint4.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            int[] array = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            Console.WriteLine("* Исходный массив:                                                        *");
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            
+            int res = ds.GetSumOddArrEl(array);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма нечетных элементов массива = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
