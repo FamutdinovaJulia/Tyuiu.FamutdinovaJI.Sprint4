6d26550d4f7031c2cebc399dafc6d84ad7484335
FamutdinovaJulia <i212195.73@gmail.com>
2025-01-22 07:54:24 +0500

    task6

---
+++ Tyuiu.FamutdinovaJI.Sprint4.Task6.V15/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.FamutdinovaJI.Sprint4.Task6.V15.Lib;
+namespace Tyuiu.FamutdinovaJI.Sprint4.Task6.V15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            var str = new string[] { "Чикаго", "Хьюстон", "Феникс", "Филадельфия", "Сан-Антонио", "Сан-Диего", "Даллас" };
+
+            Console.WriteLine("* Исходный массив: ");
+            for (int i = 0; i <= str.Length - 1; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+
+            int res = ds.Calculate(str);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество элементов, длина которых < 7 = " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
